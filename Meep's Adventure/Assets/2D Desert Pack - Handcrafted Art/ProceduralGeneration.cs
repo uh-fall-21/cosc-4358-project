e04762efd0f706a51f239eb59f2c0fb3b227faf6
s1dekick101 <nicbrown16@gmail.com>
2021-10-07 20:30:24 -0500

    some updates

---
+++ Meep's Adventure/Assets/2D Desert Pack - Handcrafted Art/ProceduralGeneration.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+
+    [SerializeField] int width;
+    [SerializeField] GameObject desertplatformPuzzle;
+
+    // Start is called before the first frame update
+    void Start() {
+        Generation();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void Generation() {
+        for(int x = 0; x < width ; ++x){
+
+        }
+    }
+}
