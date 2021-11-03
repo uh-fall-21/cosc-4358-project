adf98076eb5f4f08ad1d27f3ec60921d4d608131
Justin Do <70823850+jhdo1625@users.noreply.github.com>
2021-11-02 22:35:59 -0500

    Movement Script V2

--- Meep's Adventure/Assets/Scripts/PlayerControllerV2.cs
+++ Meep's Adventure/Assets/Scripts/PlayerControllerV2.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerV2 : MonoBehaviour
 {
 
     private float movementInputDirection;
