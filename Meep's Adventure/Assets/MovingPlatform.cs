482e386731f7feab800752f3e54fb770b75292b1
s1dekick101 <nicbrown16@gmail.com>
2021-11-11 14:42:42 -0600

    Platform Movement Code

--- Meep's Adventure/Assets/MovingPlatform.cs
+++ Meep's Adventure/Assets/MovingPlatform.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float min = 2f;
+    public float max = 3f;
+
+    // Use this for initialization
     void Start()
     {
-        
+
+        min = transform.position.x;
+        max = transform.position.x + max;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+
     }
 }
