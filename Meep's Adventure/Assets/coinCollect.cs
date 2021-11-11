6b4edac3496cf9e0e4fbb055988a628a1b9b8f02
bree-dot <77509576+bree-dot@users.noreply.github.com>
2021-11-11 13:16:29 -0600

    adding in coins

---
+++ Meep's Adventure/Assets/coinCollect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class coinCollect : MonoBehaviour
+{
+    private float coins = 0;
+
+    public TextMeshProUI textcoin;  
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "coin")
+        {
+            coins++;
+            textcoin.text=coins.ToString();
+        }
+    }
+}
